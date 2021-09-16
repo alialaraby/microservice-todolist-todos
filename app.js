cae8c27c5b770f56843d888060d3305b692d95a9
ali alaraby <alisakralaraby@gmail.com>
2021-09-16 13:00:06 +0200

    refactored port listening

--- app.js
+++ app.js
@@ -10,9 +10,10 @@ require('./startup/routes')(app, debug)
 
 
 //app listening, home request and establish db connection
-debug('listening on port ...');
-app.listen(process.env.PORT || 3000, () => {
-    debug(`listening on port: ${process.env.PORT || 3000}`);
-});
+// debug('listening on port ...');
+// app.listen(process.env.PORT || 3000, () => {
+//     debug(`listening on port: ${process.env.PORT || 3000}`);
+// });
+require('./startup/port')(app, debug);
 
 module.exports = { app: app, debug: debug };
