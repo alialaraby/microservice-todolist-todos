56213221f54c1c50be32e4a410ea9206ecc3d7f8
ali alaraby <alisakralaraby@gmail.com>
2021-09-23 12:53:04 +0200

    added CORS handling to startup modules

--- app.js
+++ app.js
@@ -2,23 +2,12 @@
 const express = require('express');
 const app = express();
 const debug = require('debug')('app:all'); // condotional log, choose to show these log or not from env
-const cors = require('cors');
-
-app.use(cors())
-app.options('*' , cors());
-app.use((req, res, next)=> {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', true);
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    next();
-})
 
 require('dotenv').config(); // to read vars from .env file
 require('./startup/config')(app, debug);
 require('./startup/db')(debug)
 require('./startup/routes')(app, debug)
-
 require('./startup/port')(app, debug);
+require('./startup/cors')(app);
 
 module.exports = { app: app, debug: debug };
